df5c6219336245085b771e778fc3c1e149833c2a
Alex Brenneman <alexqbrenneman@gmail.com>
2019-03-13 23:02:53 -0500

    added database to signup. working on login.

--- main.py
+++ main.py
@@ -1,81 +1,90 @@
-from flask import Flask, request, redirect, render_template, session
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_sqlalchemy import SQLAlchemy
+
 
 
 app = Flask (__name__)
 app.config['DEBUG'] = True
-
-
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://chimtech:chimtech@localhost:8889/chimtech'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'f8wv3w2f>v9j4sEuhcNYydAGMzzZJgkGgyHE9gUqaJcCk^f*^o7fQyBT%XtTvcYM'
 
 
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
 
-@app.route("/signup")
-def index():
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
     
-    return render_template('signup.html')
 
-def length(parameter):
-    if len(parameter)> 2 and len(parameter)<21:
-        return True
-    else:
-        return False
 
-def symbol(parameter):
-    if parameter.count("@") == 1 and parameter.count(".") ==1 :
-        return True
-    else:
-        return False
-
-# Check for spaces in function, returning True if there are no spaces
-def no_space(parameter):
-    if parameter.count(" ") == 0:
-        return True    
-    else:
-        return False
-
-@app.route("/login")
-def login():
-    return render_template('login.html')
-
-@app.route("/service")
-def service():
-    return render_template('service.html')
-
-@app.route('/validation', methods=['POST'])
-def validation():   
-
-    username = request.form['username']
-    password = request.form['password']
-    verifypassword = request.form['verifypassword']
-    email = request.form['email']
- 
-    if length(username) and no_space(username):
-        valid_username = ""
-    else:
-        valid_username = "Not a valid username"
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    valid_username  = ''
+    valid_password = ''
+    username = ''
     
-    if length(password) and no_space(password) and password == verifypassword:
-        valid_password = ""
-    else: 
-        valid_password = "Not a valid password"
-        
-    if length(email) and symbol(email) and no_space(email) or email == "":
-        valid_email = ""
-    else:
-        valid_email = "Not a valid email"
 
-    if valid_username=="" and valid_password=="" and valid_email=="":
-        return redirect('/welcome?username='+username)
-    else:
-        return render_template(("index.html"),username=username, email=email, valid_username=valid_username, valid_password=valid_password, valid_email = valid_email)
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
         
+
+        existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+
+            if len(username) < 4:
+                valid_username = "Not a valid username"
+            
+            if len(password) < 4 or password != verify:
+                valid_password = "Not a valid password"
+
+            if valid_username=="" and valid_password=="":
+                new_user = User(username = username, password = password)
+                db.session.add(new_user)
+                db.session.commit()
+                session['username'] = username
+                return redirect('/welcome')
+                
+        else:
+            valid_username = 'Duplicate user'
+
+    return render_template('signup.html', valid_username=valid_username, username=username , valid_password=valid_password)
+
+
 @app.route('/welcome')
 def welcome():   
     username=request.args.get('username')
     return render_template(('welcome.html'),username = username)
 
-app.run()
+@app.route('/login', methods= (['GET','POST']))
+def login():
+    valid_username  = ''
+    valid_password = ''
+    username = ''
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username 
+            return redirect('/welcome')
+        if not user:
+            valid_username = "Not a valid username"
+        if user and not user.password == password:
+            valid_password = "Not a valid password"
+            
+        
+            return render_template('login.html', valid_username=valid_username, username=username , valid_password=valid_password)
+
+
+    
+if __name__ == '__main__':
+    app.run()
