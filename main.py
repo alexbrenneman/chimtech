e00b0dcf8f44b2726b829696fd3ebc62d5e86e9e
Alex Brenneman <alexqbrenneman@gmail.com>
2019-04-09 01:43:26 -0500

    added table to add_companies,added address city state zipcode and appliance to form on signup, added link to signup on login.html, added address city state zip and appliance to welcome and profile added comments on main.py, cleaned up unused templates and stuff in main.py

--- main.py
+++ main.py
@@ -11,25 +11,34 @@ db = SQLAlchemy(app)
 app.secret_key = 'f8wv3w2f>v9j4sEuhcNYydAGMzzZJgkGgyHE9gUqaJcCk^f*^o7fQyBT%XtTvcYM'
 
 
-
+# Main landing page (home page)----------------------------------------------------------------------
 @app.route("/")
 def index():
     error = False
     return render_template("index.html")
 
 
+# This is setup for the User table in the MySQL database---------------------------------------------
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
     email = db.Column(db.String(150))
+    address = db.Column(db.String(200))
+    city = db.Column(db.String(100))
+    state = db.Column(db.String(100))
+    zipcode = db.Column(db.Integer)
+    appliance = db.Column(db.String(100))
 
 
+
+# Check for proper symbols for email validation-------------------------------------------------------
 def symbol(parameter):
     if parameter.count("@") == 1 and parameter.count(".") ==1 :
         return True
     else:
         return False
+
 
 # Check for spaces in function, returning True if there are no spaces
 def no_space(parameter):
@@ -37,11 +46,9 @@ def no_space(parameter):
         return True    
     else:
         return False
+     
 
-def mysession():
-     mysession = session.get('username', None)
-
-
+# This is the signup method ------------------------------------------------------------------------
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     valid_username  = ''
@@ -49,16 +56,29 @@ def signup():
     username = ''
     email = ''
     valid_email = ''
-    
-    
+    address = ''
+    city = ''
+    state = ''
+    zipcode = ''
+    appliance = ''
+    valid_address = ''
+    valid_city = ''
+    valid_state = ''
+    valid_zipcode = ''
+    valid_appliance = ''
 
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         verify = request.form['verify']
         email = request.form['email']
-
+        address = request.form['address']
+        city = request.form['city']
+        state = request.form['state']
+        zipcode = request.form['zipcode']
+        appliance = request.form['appliance']
         existing_user = User.query.filter_by(username=username).first()
+
         if not existing_user:
 
             if len(username) < 1:
@@ -71,21 +91,40 @@ def signup():
                 valid_email = ""
             else:
                 valid_email = "Not a valid email"
+
+            if len(address) < 1:
+                valid_address = "Not a valid address"
+
+            if len(city) < 1:
+                valid_city = "Not a valid city"
+
+            if len(state) < 1:
+                valid_state = "Not a valid state"
+
+            if len(zipcode) <= 4:
+                valid_zipcode = "Not a valid zip code"
+
+            if len(appliance) < 1:
+                valid_appliance = "Not a valid appliance"
         else:
             valid_username = 'Duplicate user'
+
         
 
-        if valid_username=="" and valid_password=="" and valid_email=="":
-            new_user = User(username = username, password = password, email = email)
+        if valid_username=="" and valid_password=="" and valid_email=="" and valid_address=="" and valid_city=="" and valid_state=="" and valid_zipcode=="" and valid_appliance=="":
+            new_user = User(username = username, password = password, email = email, address = address, city = city, state = state, zipcode = zipcode, appliance = appliance)
             db.session.add(new_user)
             db.session.commit()
             session['username'] = username
-            return render_template('/welcome.html',username = username, email = email)
+            return render_template('/welcome.html',username = username, email = email, address = address, city = city, state = state, zipcode = zipcode, appliance = appliance )
                 
         
 
-    return render_template('signup.html', valid_username=valid_username, username=username , valid_password=valid_password , email=email)
+    return render_template('signup.html', valid_username=valid_username, username=username , valid_password=valid_password , email=email, address = address, city = city, state = state, zipcode = zipcode, appliance = appliance, valid_address = valid_address, valid_city = valid_city, valid_state = valid_state, valid_zipcode = valid_zipcode, valid_appliance = valid_appliance)
 
+
+
+# This is the user profile page and how the user will get back to their personalized page--------------------------------------------------
 @app.route('/profile', methods=['POST','GET'])
 def profile():
     if "username" in session:
@@ -95,12 +134,16 @@ def profile():
         
     return render_template(("/profile.html"),user=user)
 
+
+# Welcome landing page after user either signs up or logs in------------------------------------------------------
 @app.route('/welcome', methods=['POST', 'GET'])
 def welcome():   
     username = request.form.get('username')
     email = request.form.get('email')
     return render_template(('welcome.html'),username = username, email = email)
 
+
+# This is the login method----------------------------------------------------------------------------------------
 @app.route('/login', methods= ['GET','POST'])
 def login():
     error = False
@@ -108,6 +151,12 @@ def login():
     valid_password = 'Not valid password'
     username = ''
     email=''
+    address = ''
+    city = ''
+    state = ''
+    zipcode = ''
+    appliance = ''
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -119,34 +168,14 @@ def login():
             if user.username == username and user.password == password:
                 session['username'] = username 
                 error = False
-                return render_template('/welcome.html', username = username, email = user.email)
+                return render_template('/welcome.html', username = username, email = user.email,  address = user.address, city = user.city, state = user.state, zipcode = user.zipcode, appliance = user.appliance)
         
         
            
     return render_template('login.html', valid_username=valid_username, valid_password=valid_password, error=False)
 
 
-@app.route('/reviews')
-def service():
-    return render_template('reviews.html')
-
-@app.route('/cleaning_reviews')
-def cleaning_reviews():
-    return render_template('cleaning-reviews.html')
-
-@app.route('/restoration_reviews')
-def restoration_reviews():
-    return render_template('restoration_reviews.html')
-
-@app.route('/inspection_reviews')
-def inspection_reviews():
-    return render_template('inspection_reviews.html')
-
-@app.route('/construction_reviews')
-def construction_reviews():
-    return render_template('construction_reviews.html')
-
-
+# Logout deletes the current session--------------------------------------------------------------------------------
 @app.route('/logout')
 def logout():
     del session['username']
@@ -154,10 +183,9 @@ def logout():
 
 
 
-        
-  
 
 
+# This is setup for the Companies table in the MySQL database--------------------------------------------- 
 class Companies(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True)
@@ -168,6 +196,8 @@ class Companies(db.Model):
     site = db.Column(db.String(120))
     rating = db.Column(db.Integer)
 
+
+# This is how users add new companies to our database and then show them at '/companies'---------------------
 @app.route('/add_companies', methods = ['POST'])
 def add_companies():
     valid_name = ""
@@ -184,8 +214,6 @@ def add_companies():
     site = ""
 
     existing_company = Companies.query.filter_by(name=name).first()
-        
-   
         
     name = request.form['name']
     street = request.form['street']
@@ -213,11 +241,6 @@ def add_companies():
     if len(site) < 2:
         valid_site = "Not a valid website"
 
-    
-    
-
-        
-
 
     if valid_name=="" and valid_street=="" and valid_city=="" and valid_zip_code=="" and valid_phone == "" and valid_site == "":
         new_companies = Companies(name = name, street = street, city = city, zip_code = zip_code, phone=phone, site=site, rating=0)
@@ -227,11 +250,14 @@ def add_companies():
     else:   
         return render_template("add_companies.html", valid_city =valid_city, valid_name=valid_name, valid_street=valid_street, valid_zip_code=valid_zip_code, valid_phone=valid_phone, valid_site=valid_site)
 
+
+# This method is used to show the add_companies.html template------------------------------------------------
 @app.route('/add_companies', methods = ['GET'])
 def add_temp():
     return render_template("add_companies.html")
 
 
+# This method makes it a requirement to be logged in before you can see the company list or like companies------
 @app.before_request
 def require_login():
     allowed_routes = ['login', 'signup', 'index']
@@ -239,10 +265,7 @@ def require_login():
         return redirect('/login')
 
 
-
-    
-
-
+# This method shows our current company list along with the user rating system------------------------------------
 @app.route("/companies", methods=["POST","GET"])
 def companies():
     rating =""
